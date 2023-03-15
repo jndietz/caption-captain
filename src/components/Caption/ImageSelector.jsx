6419fea1cd133282569f7762a84e8eb5e6b2142d
Jared Dietz <jdietz@fnni.com>
2023-03-15 15:49:00 -0500

    i think it's fixed?

--- src/components/Caption/ImageSelector.jsx
+++ src/components/Caption/ImageSelector.jsx
@@ -1,7 +1,7 @@
+import { Image } from '@mantine/core';
 import { Carousel } from '@mantine/carousel';
 
 export const ImageSelector = () => {
-
     const images = [
         "https://m.media-amazon.com/images/M/MV5BZGM0YjhkZmEtNGYxYy00OTk0LThlNDgtNGQzM2YwNjU0NDQzXkEyXkFqcGdeQXVyMTU3ODQxNDYz._V1_FMjpg_UX1000_.jpg",
         "https://ntvb.tmsimg.com/assets/assets/516020_v9_bc.jpg?w=270&h=360"
@@ -14,9 +14,8 @@ export const ImageSelector = () => {
     ));
 
     return (
-        <Carousel maw={320} mx="auto" withIndicators>
+        <Carousel maw={512} mah={768} mx="auto" withIndicators>
             {slides}
         </Carousel>
     );
-
 }
